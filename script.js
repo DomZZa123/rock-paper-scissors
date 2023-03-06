98f8d5957905df65f889ed9508ebd1ba5402e77f
DomZZa123 <katicdomagoj1511@gmail.com>
2023-03-05 19:33:37 -0800

    getComputerChoice

---
+++ script.js
@@ -0,0 +1,15 @@
+//COMPUTER CHOICE
+
+function getComputerChoice() {
+
+    let randomNumber = Math.ceil(Math.random() * 3);
+
+    if (randomNumber === 1) {
+        return "Rock";
+    } else if (randomNumber === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
