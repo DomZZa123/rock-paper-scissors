824ffd955947e99789de29bcf8e7f7eb103947dc
DomZZa123 <katicdomagoj1511@gmail.com>
2023-03-11 04:32:16 -0800

    Round done

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 //COMPUTER CHOICE
 
 function getComputerChoice() {
-
-    let randomNumber = Math.ceil(Math.random() * 3);
-
+    let randomNumber = Math.ceil(Math.random() * 3)
     if (randomNumber === 1) {
-        return "Rock";
+        return "rock"
     } else if (randomNumber === 2) {
-        return "Paper";
+        return "paper"
     } else {
-        return "Scissors";
+        return "scissors"
     }
 }
+
+let computerChoice = getComputerChoice();
+
+
+//PLAYER CHOICE
+
+let getPlayerChoice = prompt("What do you choose?");
+let playerChoice = getPlayerChoice.toLowerCase();
+
+console.log(playerChoice)
+console.log(computerChoice)
+
+//ROUND
+
+function playRound(playerChoice, computerChoice) {
+    if ((playerChoice === `rock` && computerChoice === `scissors`) ||
+        (playerChoice === `paper` && computerChoice === `rock`) ||
+        (playerChoice === `scissors` && computerChoice === `paper`)) {
+        return "WIN"
+    } else if(playerChoice === computerChoice) {
+        return "DRAW"
+    } else {
+        return "LOSS"
+    }
+}
+
+console.log(playRound(playerChoice, computerChoice))
+
+//GAME
 
